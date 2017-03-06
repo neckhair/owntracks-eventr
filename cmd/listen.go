42d5785e32f388d98000429f40311d9a58b2e1bf
Philippe Hässig <phil@nine.ch>
2017-03-06 20:51:01 +0100

    Move config into struct

--- cmd/listen.go
+++ cmd/listen.go
@@ -8,22 +8,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var url string
-var filename string
+type listenCfg struct {
+	Url      string
+	Filename string
+}
+
+var config = listenCfg{}
 
 var listenCmd = &cobra.Command{
 	Use:   "listen",
 	Short: "Listen for events and write them into a file",
 	Long:  `Listen for events and write them into a file line by line.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("--> Listening on %s for events, write to %s\n\n", url, filename)
-		listener.Listen(url)
+		fmt.Printf("--> Listening for MQTT events\n")
+		fmt.Printf("Server:\t%s\n", config.Url)
+		fmt.Printf("Output:\t%s\n\n", config.Filename)
+
+		listener.Listen(config.Url)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(listenCmd)
 
-	listenCmd.Flags().StringVarP(&url, "url", "u", "tcp://localhost:1883", "Connection string")
-	listenCmd.Flags().StringVarP(&filename, "output", "o", "eventlog.txt", "Path to destination file")
+	listenCmd.Flags().StringVarP(&config.Url, "url", "u", "tcp://localhost:1883", "Connection string")
+	listenCmd.Flags().StringVarP(&config.Filename, "output", "o", "eventlog.txt", "Path to destination file")
 }
